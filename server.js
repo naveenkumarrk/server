19d0cdae0413b231da433670be17cbeeb242fdd4
naveenrk7 <111naveenkumarrk@gmail.com>
2025-02-19 22:54:39 +0530

    commit

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Serve static files correctly
-app.use(express.static("public"));
+// Serve static files using an absolute path
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 const booksRoute = require("./routes/book.route.js");
@@ -20,7 +20,7 @@ const recommendationRoutes = require("./routes/recommendation.route.js");
 app.use("/books", booksRoute);
 app.use("/recommendation", recommendationRoutes);
 
-// Serve HTML pages correctly
+// Serve HTML pages
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
@@ -32,12 +32,13 @@ app.get("/recommendation", (req, res) => {
 mongoose.connect("mongodb+srv://111naveenkumarrk:naveenRose1@backenddb.lbjki.mongodb.net/BooksAPI?retryWrites=true&w=majority&appName=BackendDB")
     .then(() => {
         console.log("Database is connected");
-        app.listen(3000, () => {
-            console.log("Server is running on port 3000");
+        const port = process.env.PORT || 3000;
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
         });
     })
-    .catch(() => {
-        console.log("Error occurred");
+    .catch((err) => {
+        console.error("Error occurred connecting to MongoDB:", err);
     });
 
-module.exports = app; // Required for Vercel
+module.exports = app; 
